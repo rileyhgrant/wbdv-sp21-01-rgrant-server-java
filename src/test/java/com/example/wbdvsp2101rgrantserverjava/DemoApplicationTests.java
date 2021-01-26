1abfacc812d9b1c609eee24783c33bb0b95413d3
Riley Grant <grantrileyh@gmail.com>
2021-01-26 09:25:27 -0500

    Added basic home page, login

---
+++ src/test/java/com/example/wbdvsp2101rgrantserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2101rgrantserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
